f644bbef92ca42576fb8e45c8752dbd8c54548fb
patrickpa <patrickabrahao.pa@gmail.com>
2019-02-18 18:52:33 -0300

    Initial commit - ROS Begginers Tutorial - 9/20 Done

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/patrick/ros_study/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/patrick/ros_study/catkin_ws/devel;/home/patrick/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/patrick/ros_study/catkin_ws/devel;/home/patrick/catkin_ws/devel;/opt/ros/kinetic" != "" else []
